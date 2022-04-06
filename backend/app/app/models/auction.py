ebda1f2d52d37a159c0d783d18ea4017d56ba305
Kimba SABI N'GOYE <kimbasabingoye@gmail.com>
2022-04-06 23:14:45 +0100

    Auction and Bid models added

---
+++ backend/app/app/models/auction.py
@@ -0,0 +1,28 @@
+from email.policy import default
+from typing import TYPE_CHECKING
+from datetime import datetime
+
+from sqlalchemy import Column, ForeignKey, Integer, Boolean, String
+from sqlalchemy.orm import relationship
+
+from app.db.base_class import Base
+
+if TYPE_CHECKING:
+    from .user import User  # noqa: F401
+
+
+class Auction(Base):
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True)
+    description = Column(String, index=True)
+    owner_id = Column(Integer, ForeignKey("user.id"))
+    is_published = Column(Boolean, default=False)
+    is_active = Column(Boolean, default=False)
+    cycle_duration = Column(Integer, default=5)     # duration in minutes
+    max_cycle_number = Column(Integer, default=3)
+    creation_date = Column(String, default=datetime.now())
+    start_date = Column(String)
+    end_date = Column(String)
+
+    owner = relationship("User", back_populates="auctions")
+    items = relationship("Item", back_populates="auction", cascade="all, delete", passive_deletes=True)
--- backend/app/app/models/user.py
+++ backend/app/app/models/user.py
@@ -11,9 +11,13 @@ if TYPE_CHECKING:
 
 class User(Base):
     id = Column(Integer, primary_key=True, index=True)
-    full_name = Column(String, index=True)
+    first_name = Column(String, index=True)
+    last_name = Column(String, index=True)
     email = Column(String, unique=True, index=True, nullable=False)
     hashed_password = Column(String, nullable=False)
     is_active = Column(Boolean(), default=True)
     is_superuser = Column(Boolean(), default=False)
+
+    auctions = relationship("Auction", back_populates="owner")
+    bids = relationship("Bid", back_populates="owner")
     items = relationship("Item", back_populates="owner")
